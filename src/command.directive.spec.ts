6a93bd3777aa62a2a1112dc71345d254dfe6bf69
Stephen Lautier <stephen.lautier@outlook.com>
2016-07-20 04:01:16 +0200

    test(command directive): added test specs for directive (no tests implemented)

---
+++ src/command.directive.spec.ts
@@ -0,0 +1,49 @@
+import "rxjs/Rx";
+
+import { Command, ICommand, CommandDirective } from "./index";
+
+describe("CommandDirectiveSpecs", () => {
+
+	xdescribe("given command isExecuting is set to true", () => {
+		xit("should have element disabled", () => {
+
+		});
+
+		xit("should have executingCssClass added", () => {
+
+		});
+
+		xdescribe("when isExecuting has been finished", () => {
+			xit("should have element enabled", () => {
+
+			});
+
+			xit("should have executingCssClass removed", () => {
+
+			});
+		});
+	});
+
+	xdescribe("given command canExecute is set to true", () => {
+		xit("should have element enabled", () => {
+
+		});
+	});
+
+	xdescribe("given command canExecute is set to false", () => {
+		xit("should have element disabled", () => {
+
+		});
+	});
+
+	xdescribe("given commandOptions are provided", () => {
+		xdescribe("when isExecuting is set true", () => {
+			xit("should have executingCssClass added with the provided option", () => {
+
+			});
+		});
+
+	});
+
+
+});
