7492fb156d3c3c38d57dd9b5b0cb7ed81ea5d313
Stephen Lautier <stephen.lautier@outlook.com>
2016-07-31 15:25:11 +0200

    commented out command.directive specs

--- src/command.directive.spec.ts
+++ src/command.directive.spec.ts
@@ -1,102 +1,105 @@
-import "rxjs/Rx";
-import {inject, addProviders, async} from "@angular/core/testing";
-import {TestComponentBuilder, ComponentFixture} from "@angular/compiler/testing";
-import {Component, provide} from "@angular/core";
+// import {Observable} from "rxjs/Rx";
+// import {inject, addProviders, async} from "@angular/core/testing";
+// import {TestComponentBuilder, ComponentFixture} from "@angular/compiler/testing";
+// import {Component, provide} from "@angular/core";
 
-import {Command, ICommand, CommandDirective} from "./index";
+// import {Command, ICommand, CommandDirective} from "./index";
 
-@Component({
-	template: `<button class="btn" [command]="saveCmd" >Save</button>`,
-	directives: [
-		CommandDirective
-	]
-})
-class TestContainer {
-	saveCmd = new Command(() => {});
-}
+// @Component({
+// 	template: `<button class="btn" [command]="saveCmd" >Save</button>`,
+// 	directives: [
+// 		CommandDirective
+// 	]
+// })
+// class TestContainer {
+// 	saveCmd = new Command(() => Observable.of("yey").delay(2000), null, true);
+// }
 
-@Component({
-	template: `<button class="btn" [command]="emptyCmd" >Save</button>`,
-	directives: [
-		CommandDirective
-	]
-})
-class NullCommandTestContainer {
+// @Component({
+// 	template: `<button class="btn" [command]="emptyCmd" >Save</button>`,
+// 	directives: [
+// 		CommandDirective
+// 	]
+// })
+// class NullCommandTestContainer {
 
-}
+// }
 
-describe("CommandDirectiveSpecs", () => {
+// describe("CommandDirectiveSpecs", () => {
 
-	let testComponentBuilder: TestComponentBuilder;
+// 	let testComponentBuilder: TestComponentBuilder;
 
-	beforeEach(inject([
-		TestComponentBuilder
-	], (
-		_testComponentBuilder: TestComponentBuilder
-	) => {
-			testComponentBuilder = _testComponentBuilder;
-		})
-	);
+// 	beforeEach(inject([
+// 		TestComponentBuilder
+// 	], (
+// 		_testComponentBuilder: TestComponentBuilder
+// 	) => {
+// 			testComponentBuilder = _testComponentBuilder;
+// 		})
+// 	);
 
-	describe("given an undefined command", () => {
-		xit("should throw error", async(() => {
-			testComponentBuilder
-				.createAsync(NullCommandTestContainer)
-				.then((fixture: ComponentFixture<NullCommandTestContainer>) => {
-					expect(() => {
-						fixture.detectChanges();
-					}).toThrowError("[commandDirective] command should be defined!");
-				});
+// 	describe("given an undefined command", () => {
+// 		xit("should throw error", async(() => {
+// 			testComponentBuilder
+// 				.createAsync(NullCommandTestContainer)
+// 				.then((fixture: ComponentFixture<NullCommandTestContainer>) => {
+// 					expect(() => {
+// 						fixture.detectChanges();
+// 					}).toThrowError("[commandDirective] command should be defined!");
+// 				});
 
-		}));
-	});
+// 		}));
+// 	});
 
-	describe("given command isExecuting is set to true", () => {
-		it("should have element disabled", async(() => {
-			// testComponentBuilder
-			// 	.createAsync(TestContainer)
-			// 	.then((fixture: ComponentFixture<TestContainer>) => {
-			// 		fixture.detectChanges();
-			// 		let element = fixture.debugElement.nativeElement.querySelector("button");
-			// 		expect(element).toHaveClass("btn");
-			// 	});
-		}));
+// 	describe("given command isExecuting is set to true", () => {
+// 		xit("should have element disabled", async(() => {
+// 			testComponentBuilder
+// 				.createAsync(TestContainer)
+// 				.then((fixture: ComponentFixture<TestContainer>) => {
+// 					fixture.detectChanges();
+// 					let container = fixture.componentInstance as TestContainer;
+// 					let element = fixture.debugElement.nativeElement.querySelector("button");
+// 					element.click();
+// 					fixture.detectChanges();
+// 					expect(element).toBeDisabled();
+// 				});
+// 		}));
 
-		xit("should have executingCssClass added", () => {
+// 		xit("should have executingCssClass added", () => {
 
-		});
+// 		});
 
-		xdescribe("when isExecuting has been finished", () => {
-			xit("should have element enabled", () => {
+// 		xdescribe("when isExecuting has been finished", () => {
+// 			xit("should have element enabled", () => {
 
-			});
+// 			});
 
-			xit("should have executingCssClass removed", () => {
+// 			xit("should have executingCssClass removed", () => {
 
-			});
-		});
-	});
+// 			});
+// 		});
+// 	});
 
-	xdescribe("given command canExecute is set to true", () => {
-		xit("should have element enabled", () => {
+// 	xdescribe("given command canExecute is set to true", () => {
+// 		xit("should have element enabled", () => {
 
-		});
-	});
+// 		});
+// 	});
 
-	xdescribe("given command canExecute is set to false", () => {
-		xit("should have element disabled", () => {
+// 	xdescribe("given command canExecute is set to false", () => {
+// 		xit("should have element disabled", () => {
 
-		});
-	});
+// 		});
+// 	});
 
-	xdescribe("given commandOptions are provided", () => {
-		xdescribe("when isExecuting is set true", () => {
-			xit("should have executingCssClass added with the provided option", () => {
+// 	xdescribe("given commandOptions are provided", () => {
+// 		xdescribe("when isExecuting is set true", () => {
+// 			xit("should have executingCssClass added with the provided option", () => {
 
-			});
-		});
+// 			});
+// 		});
 
-	});
+// 	});
 
 
-});
+// });
