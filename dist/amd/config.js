c43dcd56e163b28f538905dba42493b14fd90903
Stephen Lautier <stephen.lautier@outlook.com>
2017-11-27 23:29:26 +0100

    chore(all): remove karma, cleanup deps, setup ng-packagr

--- dist/amd/config.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "@angular/core"], function (require, exports, core_1) {
-    "use strict";
-    exports.COMMAND_DEFAULT_CONFIG = {
-        executingCssClass: "executing",
-    };
-    exports.COMMAND_CONFIG = new core_1.OpaqueToken("Command Config");
-});
-
-//# sourceMappingURL=config.js.map
