dd0c2b81d95f1f76afe86e78a21a3a324b4aec4a
Stephen Lautier <stephen.lautier@outlook.com>
2019-09-13 00:49:39 +0200

    change to cards from fieldset

--- ngx.command/projects/example-app/src/app/app.module.ts
+++ ngx.command/projects/example-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 import { CommandModule } from "@ssv/ngx.command";
 
 import { AppComponent } from "./app.component";
@@ -14,6 +15,7 @@ const materialModules = [
   MatButtonModule,
   MatProgressSpinnerModule,
   MatIconModule,
+  MatCardModule,
 ];
 
 @NgModule({
